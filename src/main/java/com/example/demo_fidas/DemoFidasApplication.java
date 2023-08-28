86b6a258cc18afec5e7f3b8df5bbfb25629aa2db
fidasgabriel <gabriel.fidalgo@germinare.org.br>
2023-08-28 18:52:33 -0300

    first commit

---
+++ src/main/java/com/example/demo_fidas/DemoFidasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_fidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_fidas/DemoFidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_fidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
