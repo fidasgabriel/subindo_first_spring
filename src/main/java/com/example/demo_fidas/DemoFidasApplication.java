e211cf37b75b71837ce02cf2621affb98623c2c4
fidasgabriel <131205434+fidasgabriel@users.noreply.github.com>
2023-08-28 22:50:06 +0000

    adding hello get on project

--- src/main/java/com/example/demo_fidas/DemoFidasApplication.java
+++ src/main/java/com/example/demo_fidas/DemoFidasApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoFidasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoFidasApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Germinare Tech - Fidas";
+	} 
 
 }
